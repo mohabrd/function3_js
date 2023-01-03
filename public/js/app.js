1bf36c149b69200fd913aa3894566c3b38e40ada
mohabrd <bezrhoudimohamed@gmail.com>
2023-01-03 14:19:30 +0100

    exo2

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,21 @@
 //EXO1
 
-function nombrenaturel(n) {
-    let nbr = 0;
-    for (let i = 1; i <= n; i++) {
-      nbr += i;
-    }
-    return nbr;
-  }
+// function nombrenaturel(n) {
+//     let nbr = 0;
+//     for (let i = 1; i <= n; i++) {
+//       nbr += i;
+//     }
+//     return nbr;
+//   }
 
-console.log(nombrenaturel(5));
+// console.log(nombrenaturel(5));
+
+//EXO2
+
+function nbrPremier(nbr) {
+    for(let i = 2; i < nbr; i++)
+      if(nbr%i === 0) return false;
+    return nbr > 1;
+  }
+  
+  console.log(nbrPremier(4));
