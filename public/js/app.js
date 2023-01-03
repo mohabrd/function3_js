c45adc9c8761c33a27d5e066b83a5acbef89fc0c
mohabrd <bezrhoudimohamed@gmail.com>
2023-01-03 14:40:40 +0100

    exo3

--- public/js/app.js
+++ public/js/app.js
@@ -12,10 +12,20 @@
 
 //EXO2
 
-function nbrPremier(nbr) {
-    for(let i = 2; i < nbr; i++)
-      if(nbr%i === 0) return false;
-    return nbr > 1;
-  }
+// function nbrPremier(nbr) {
+//     for(let i = 2; i < nbr; i++)
+//       if(nbr%i === 0) return false;
+//     return nbr > 1;
+//   }
   
-  console.log(nbrPremier(4));
+//   console.log(nbrPremier(4));
+
+//EXO3
+
+function ordre(str) {
+    let animaux = ["girafe", "chien", "chat", "oiseau"];
+    let animauxtrier = animaux.sort();
+    return animauxtrier;
+}
+
+console.log(ordre());
